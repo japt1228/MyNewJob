095ea8a63e4afc2ac0a71e03ca5d4288f960819c
Julian Palacio <japt94@gmail.com>
2023-11-24 11:35:20 -0500

    Actualizacion Seccion Auth (Login - Register)

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+        {{ __('Forgot your password?') }}
     </div>
 
     <!-- Session Status -->
@@ -16,10 +16,19 @@
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
+        <div class="flex justify-between my-5">
+            <x-link :href="route('register')">
+                Register
+            </x-link>
+
+            <x-link :href="route('login')">
+                Log in
+            </x-link>
+
         </div>
+
+        <x-primary-button class="w-full justify-center">
+            {{ __('Email Password Reset Link') }}
+        </x-primary-button>
     </form>
 </x-guest-layout>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -16,6 +16,21 @@
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
+        <!-- Role -->
+        <div class="mt-4">
+            <x-input-label for="Role" :value="__('Role')" />
+
+            <select 
+                id = "role"
+                name = "role"
+                class ="w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm"
+            >
+                <option value="">-- Selecciona un Rol --</option>
+                <option value="1">Aspirante</option>
+                <option value="2">Empresa</option>
+            </select>
+        </div>
+
         <!-- Password -->
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
@@ -39,14 +54,15 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
+        <div class="flex justify-between my-5">
+            <x-link :href="route('login')">
+                Already Resgistered?
+            </x-link>
 
-            <x-primary-button class="ml-4">
-                {{ __('Register') }}
-            </x-primary-button>
         </div>
+    
+        <x-primary-button class="w-full justify-center">
+            {{ __('Register') }}
+        </x-primary-button>
     </form>
 </x-guest-layout>
