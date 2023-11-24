e110e034e65368de757ae2fe7bfb07a09327ae77
Christian Gallo <cristiangallosc@outlook.com>
2023-11-23 23:02:11 -0500

    vista crear Vacante

--- app/Http/Controllers/VacancyController.php
+++ app/Http/Controllers/VacancyController.php
@@ -6,5 +6,28 @@ use Illuminate\Http\Request;
 
 class VacancyController extends Controller
 {
-    //
+    
+    public function index(){
+        return view('Vacancies.index');
+    }
+
+    public function create(){
+        return view('Vacancies.create');
+    }
+
+    public function store(){
+
+    }
+
+    public function show($id){
+
+    }
+
+    public function edit($id){
+
+    }
+
+    public function delete(){
+
+    }
 }
