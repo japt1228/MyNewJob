2d23893f7fd58280e9460be86390bf320c9bd1e6
Julian Palacio <japt94@gmail.com>
2023-11-24 23:54:35 -0500

    Nuevo Merge

--- database/migrations/2023_11_09_041929_create_vacancies_table.php
+++ database/migrations/2023_11_09_041929_create_vacancies_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('vacancies', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            //$table->foreignId('salary_id')->constrained()->onDelete('cascade');
+            //$table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->string('company');
+            $table->date('last_day');
+            $table->text('description');
+            $table->string('image');
+            $table->integer('publish')->default(1);
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             
             $table->timestamps();
         });
